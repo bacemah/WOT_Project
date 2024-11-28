17091101de951198a06e7cbf9bf17ecbb2d929ac
Ahmed Bacem <bacemahmed@Ahmeds-MacBook-Pro.local>
2024-11-28 19:06:07 +0100

    fileStructurefix

---
+++ code/flamesense/src/main/java/me/flamesense/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package me.flamesense.repositories;
+
+
+import jakarta.data.repository.Repository;
+import jakarta.data.repository.CrudRepository;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import me.flamesense.entity.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User ,String>{
+    Optional<User> findByEmail(String email);
+    Stream<User> findAll();
+
+}
